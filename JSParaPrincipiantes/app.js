b74d835f9612013d96753808c89478deb1bc7fe3
Abraham7654 <124049257@upq.edu.mx>
2025-09-11 07:05:29 -0600

    Segundo avance

---
+++ JSParaPrincipiantes/app.js
@@ -0,0 +1,31 @@
+// Tipos de datos
+"Hello World" // string
+"Hello World" // string
+
+1000000  // number
+-2,3
+
+// boolen
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1,2,3]
+[true, false, true, false]
+
+// object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
+
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": true
+}
